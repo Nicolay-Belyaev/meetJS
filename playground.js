5a8b3ac5725cb4640d4d10d214b5afd0b6f9f13d
NicolayBelyaev <nickb2147@gmail.com>
2023-10-14 13:06:20 +0300

    homework_06

--- playground.js
+++ playground.js
@@ -1,1 +1,29 @@
-console.log(typeof NaN)
+const divContent = document.querySelector(".content");
+const someParagraph = document.createElement("p");
+someParagraph.textContent = "Some text in paragraph";
+
+const button = document.createElement('button');
+button.textContent = "Send";
+button.onclick = () => {
+    button.textContent = "Text sent!";
+}
+
+const anotherButton = document.createElement("button");
+anotherButton.state = "not-clicked";
+anotherButton.textContent = "Show text";
+anotherButton.onclick = () => {
+    if (anotherButton.state === "not-clicked") {
+        anotherButton.state = "clicked";
+        anotherButton.textContent = "Hide text";
+        divContent.append(someParagraph);
+        return;
+    }
+    anotherButton.state = "not-clicked";
+    anotherButton.textContent = "Show text";
+    divContent.removeChild(someParagraph);
+}
+
+divContent.append(button);
+divContent.append(anotherButton);
+
+
