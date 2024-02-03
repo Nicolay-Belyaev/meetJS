9e241f95286cf878eb5ce5e3368c7fdf6e0804c1
NickolayBelyaev <nickb2147@gmail.com>
2024-02-03 20:58:19 +0300

    Homework 03 version 2

--- 2_homework_03/showReviews.js
+++ 2_homework_03/showReviews.js
@@ -8,23 +8,36 @@ productsNames.forEach((product) => {
     const div = document.createElement("div");
     const headline = document.createElement("p");
     const ul = document.createElement("ul");
+    const productViews = JSON.parse(localStorage.getItem(product));
 
     div.append(headline, ul);
     div.addEventListener("click", ({target}) => {
         if (target.tagName === "P") {
-            ul.insertAdjacentHTML(renderViewsList(product));
+            if (ul.innerHTML !== "") {
+                ul.innerHTML = "";
+            } else {
+                productViews.forEach((view) => {ul.append(renderViewsList(product, view, productViews))});
+            }
         }
     });
     headline.textContent = `${product}`;
     viewsDiv.append(div);
 })
 
-const renderViewsList = (product) => {
-    const productViews = JSON.parse(localStorage.getItem(product));
+const renderViewsList = (product, view, productViews) => {
     const li = document.createElement("li");
     const span = document.createElement("span");
     const removeBtn = document.createElement("button");
 
+    span.innerText = `${view}`;
+    removeBtn.innerText = "удалить";
+    removeBtn.addEventListener("click", () => {
+        productViews.splice(productViews.indexOf(view), 1);
+        localStorage.setItem(product, JSON.stringify(productViews));
+        removeBtn.parentElement.remove();
+        if (productViews.length === 0) {localStorage.removeItem(product);}
+    });
 
-
+    li.append(span, removeBtn);
+    return li;
 }
